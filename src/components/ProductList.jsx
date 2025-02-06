2a7cb0d38c7f4a451eaa41429ee64e7fe9bb9946
unknown <rajujeelaga@gmail.com>
2025-02-06 18:16:46 +0530

    smal change

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,11 +1,11 @@
 export default function ProductList({id, name, price, category, image}){
     return(
         <li>
+            <img src={image} alt={name} />
             <span>{id}</span>
             <h2>{name}</h2>
             <strong>{price}</strong>
             <span>{category}</span>
-            <img src={image} alt={name} />
         </li>
         
     )
